e3fa868bcbb011fc4c92ac44341f0bf899e3ff4c
prathima129 <prathima.shrestha@gmail.com>
2025-08-22 06:50:45 +0000

    Done with the Day

--- server/app.js
+++ server/app.js
@@ -18,6 +18,33 @@ app.post("/students/:id",(req,res)=>{
     console.log(id)
     res.json(id)
 });
+app.post("/colleges/:collegename",(req,res)=>{
+    let colleges = req.params.collegename;
+    console.log(colleges)
+    res.json(colleges)
+});
+app.get("/link",(req,res)=>{
+    try {
+        res.redirect("https://google.com")
+    } catch (error) {
+        console.log(error)
+    }
+});
+app.get("/abc/:connect",(req,res)=>{
+    try {
+        let abc=req.params.connect;
+        res.redirect(`https://${abc}.com`);
+    } catch (error) {
+        console.log(error)
+    }
+});
+app.get("/download",(req,res)=>{
+    try {
+       res.download("./image.jpg")
+    } catch (error) {
+        console.log(error)
+    }
+});
 app.listen(PORT,()=>{
     console.log(`Server is Running at http:/localhost:${PORT}`);
 });
